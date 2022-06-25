b76f372eedc7fb75393e0440153b6f93c3f16947
Marcello C <marcello_ct@hotmail.com>
2022-06-24 22:53:19 -0300

    Created Orphanage page

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,56 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+// create map
+const map = L.map('mapid', options).setView([-27.2153213, -49.6551177], 15);
+
+// create and add  titleLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+// create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29,68],
+    popupAnchor: [170, 2]
+})
+
+// create and add marker
+L.marker([-27.2153213, -49.6551177], {icon}).addTo(map)
+
+// image gallery
+
+function selectImage(event) {
+    
+    const button = event.currentTarget
+    // console.log(button)
+
+    // remover todas as classes .active
+
+    const buttons = document.querySelectorAll(".images button")
+    // console.log(buttons)
+
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(buttonActive) {
+        buttonActive.classList.remove("active")
+    }
+
+    // selecionar a imagem clicada
+
+    // console.log(button.children)
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+    
+
+    // atualizar o container de imagem
+    imageContainer.src = image.src
+
+    //adicionar a classe .active para este botao que foi clicado
+    button.classList.add('active')
+}
