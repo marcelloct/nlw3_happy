1a1b80ac1532f5d3eea5d93163c86d882a41df17
Marcello C <marcello_ct@hotmail.com>
2022-06-29 00:29:19 -0300

    Create local server and make some changes in structure

---
+++ src/database/fakedata.js
@@ -0,0 +1,31 @@
+module.exports = [
+    {
+        id: 1,
+        name: "Lar das meninas",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis similique placeat rerum eveniet nostrum quibusdam pariatur officia eaque nisi itaque, quam, ex beatae qui quo eos numquam veniam magnam ullam?",
+        images: [
+            "https://source.unsplash.com/random?id=1",
+            "https://source.unsplash.com/random?id=2"
+        ],
+        lat: "-27.222633",
+        lng: "-49.6455874",
+        instructions: "Venha se sentir a vontade e traga muito amor e paciencia",
+        opening_hours: "Horário de visitas da 18h até 8h",
+        open_on_weekends: "1"
+
+    },
+    {
+        id: 2,
+        name: "Lar dos meninos",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis similique placeat rerum eveniet nostrum quibusdam pariatur officia eaque nisi itaque, quam, ex beatae qui quo eos numquam veniam magnam ullam?",
+        images: [
+            "https://source.unsplash.com/random?id=1",
+            "https://source.unsplash.com/random?id=2"
+        ],
+        lat: "-27.222633",
+        lng: "-49.6555874",
+        instructions: "Venha se sentir a vontade e traga muito amor e paciencia",
+        opening_hours: "Horário de visitas da 18h até 8h",
+        open_on_weekends: "0"
+    }
+]
---
+++ src/server.js
@@ -0,0 +1,21 @@
+// importar package / dependencia
+const express = require("express");
+const path = require("path");
+const pages = require('./pages.js')
+// iniciando express
+const server = express();
+
+server
+  // utilizando os arquivos estaticos
+  .use(express.static("public"))
+  //configurar template engine
+  .set('views', path.join(__dirname, "views"))
+  .set('view engine', 'hbs')
+  // rotas da aplicacao
+  .get("/", pages.index)
+  .get("/orphanage", pages.orphanage)
+  .get("/orphanages", pages.orphanages)
+  .get("/create-orphanage", pages.createOrphanage)
+
+// ligar o servidor
+server.listen(5500);
